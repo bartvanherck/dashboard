024be0b9b6f834eec47e8fb9cc9d34c1cf876465
Bart Vanherck <vherckbart@gmail.com>
2016-09-03 18:56:41 +0200

    use a html template

--- www/js/app/trafficlight/trafficlight.component.js
+++ www/js/app/trafficlight/trafficlight.component.js
@@ -1,23 +1,7 @@
 angular.
   module('trafficLight').
   component('trafficLight', {
-    template:
-      '<div class="container">' +
-      '<div class="trafficlight">' +
-      '<div class="protector"></div>' +
-      '<div class="protector"></div>' +
-      '<div class="protector"></div>' +
-      '<div class="trafficlightbody">' +
-        '<div ng-class="$ctrl.isRed() ? \'red on\' : \'red off\' "></div>' + 
-        '<div ng-class="$ctrl.isYellow() ? \'yellow on\' : \'yellow off\' "></div>' + 
-        '<div ng-class="$ctrl.isGreen() ? \'green on\' : \'green off\' "></div>' + 
-      '</div>' +
-    '</div>' +
-    '</div>' +
-    '<button type="button" ng-click="$ctrl.activate(\'red\')">Red</button>' +
-    '<button type="button" ng-click="$ctrl.activate(\'yellow\')">Yellow</button>' +
-    '<button type="button" ng-click="$ctrl.activate(\'green\')">Green</button>' +
-    '<button type="button" ng-click="$ctrl.activate(\'off\')">Off</button>',
+    templateUrl: 'js/app/trafficlight/trafficlight.template.html',
     controller: function TrafficlightController(){
         this.color = 'off';
         this.activate = function(color){
