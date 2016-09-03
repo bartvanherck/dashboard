d6e0733053a513608c931ca9a57f9dbf951e647f
Bart Vanherck <vherckbart@gmail.com>
2016-09-03 15:29:54 +0200

    testing the activate function

--- test/unit/trafficlight_Spec.js
+++ test/unit/trafficlight_Spec.js
@@ -2,11 +2,42 @@ describe('Trafficlight', function() {
     var ctrl;
     beforeEach(module('dashboardApp'));
     beforeEach(inject(function($componentController){
-      ctrl = $componentController('trafficLight');
+        ctrl = $componentController('trafficLight');
     }));
 
     it('Initial color is off', function(){
-      expect(ctrl.color).toBe("off");
+        expect(ctrl.isRed()).toBe(false);
+        expect(ctrl.isYellow()).toBe(false);
+        expect(ctrl.isGreen()).toBe(false);
+    });
+
+    it('Activate Red changes color to red', function(){
+        ctrl.activate("red");
+        expect(ctrl.isRed()).toBe(true);
+        expect(ctrl.isYellow()).toBe(false);
+        expect(ctrl.isGreen()).toBe(false);
+    });
+
+    it('Activate Green changes color to green', function(){
+        ctrl.activate("green");
+        expect(ctrl.isRed()).toBe(false);
+        expect(ctrl.isYellow()).toBe(false);
+        expect(ctrl.isGreen()).toBe(true);
+    });
+
+    it('Activate Yellow changes color to yellow', function(){
+        ctrl.activate("yellow");
+        expect(ctrl.isRed()).toBe(false);
+        expect(ctrl.isYellow()).toBe(true);
+        expect(ctrl.isGreen()).toBe(false);
+    });
+
+    it('Deactivate button with a color active resets all colors', function(){
+        ctrl.activate("yellow");
+        ctrl.activate("off");
+        expect(ctrl.isRed()).toBe(false);
+        expect(ctrl.isYellow()).toBe(false);
+        expect(ctrl.isGreen()).toBe(false);
     });
 });
 
