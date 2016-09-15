2c92709575c92b307780d2cbf4c4fa28a2f89bb8
Bart Vanherck <vherckbart@gmail.com>
2016-09-15 18:55:21 +0200

    use self iso this

--- www/js/app/trafficlight/trafficlight.component.js
+++ www/js/app/trafficlight/trafficlight.component.js
@@ -1,22 +1,24 @@
+
 angular.
-  module('trafficLight').
-  component('trafficLight', {
-    templateUrl: 'js/app/trafficlight/trafficlight.template.html',
-    controller: ['$http', function TrafficlightController($http){
-        this.color = 'off';
-        this.activate = function(color){
-            this.color = color; 
-        };
-        this.isRed = function() { return this.color === 'red'; };
-        this.isYellow = function() { return this.color === 'yellow'; };
-        this.isGreen = function() { return this.color === 'green'; };
-        this.fetchData = function() {
+     module('trafficLight').
+     component('trafficLight', {
+        templateUrl: 'js/app/trafficlight/trafficlight.template.html',
+        controller: ['$http', function TrafficlightController($http) {
+            "use strict";
+            
             var self = this;
-            data = {"id": 1,"project": "cronos","status": "green" };
-            $http.get('http://localhost:3000/status/cronos').then(function(response) {
-                data = response.data;
-                self.color = data.status;
-            });
-        };
-    }]
-  });
+            self.color = 'off';
+            self.activate = function (color) {
+                self.color = color;
+            };
+            self.isRed = function () { return self.color === 'red'; };
+            self.isYellow = function () { return self.color === 'yellow'; };
+            self.isGreen = function () { return self.color === 'green'; };
+            self.fetchData = function () {
+                $http.get('http://localhost:3000/status/cronos').then(function (response) {
+                    var data = response.data;
+                    self.color = data.status;
+                });
+            };
+        }]
+    });
